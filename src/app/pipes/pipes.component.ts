1511766fcbb15b40fdaad0816967db25a2304a89
Job Karani <jobkarani084j@gmail.com>
2022-06-06 17:25:42 +0300

    number pipes

--- src/app/pipes/pipes.component.ts
+++ src/app/pipes/pipes.component.ts
@@ -8,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
   <h2>{{message | titlecase}}</h2>
   <h2>{{name | slice:2:4 }}</h2>
   <h2>{{person| json }}</h2>
+
+  <h2>{{5.678 | number:'1.2-3'}}</h2>
+  <h2>{{5.678 | number:'3.4-5'}}</h2>
+  <h2>{{5.678 | number:'3.1-2'}}</h2>
   `,
   styles: []
 })
